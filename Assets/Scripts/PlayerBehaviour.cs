02d282327b78dedd92e880f34de23f150d14e8ad
Mihail Shterev <mdshterev@gmail.com>
2024-04-21 01:01:14 +0300

    kam predniq push

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -45,7 +45,7 @@ public class Player : MonoBehaviour
 
     private bool IsGrounded()
     {
-        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        return Physics2D.OverlapCircle(groundCheck.position,1f, groundLayer);
     }
 
     private void Flip()
