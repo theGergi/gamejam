f1d26a1bb1f372fdbbaa9e9320d84267e4be2943
RimoLimo <mlazarovjr@abv.bg>
2024-04-20 14:41:40 +0200

    Main menu functionality
    Added main menu basic functionality

---
+++ Assets/Scripts/MainMenuEvents.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MainMenuEvents : MonoBehaviour
+{
+    private UIDocument _document;
+
+    private Button _button;
+
+    private void Awake()
+    {
+        _document = GetComponent<UIDocument>();
+           
+        _button = _document.rootVisualElement.Q("StartGameButton") as Button;
+        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+    }
+
+    private void OnDisable()
+    {
+        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+    }
+
+    private void OnPlayGameClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the Start Button");
+    }
+}
