5e3d97fab0d5654ae1ee28abe231d218108c70d5
RimoLimo <mlazarovjr@abv.bg>
2024-04-20 16:08:25 +0200

    Main Menu Buttons optimisation
    Start and Exit buttons optimised

--- Assets/Scripts/MainMenuEvents.cs
+++ Assets/Scripts/MainMenuEvents.cs
@@ -7,23 +7,32 @@ public class MainMenuEvents : MonoBehaviour
 {
     private UIDocument _document;
 
-    private Button _button;
+    private Button _start_button;
+    private Button _exit_button;
 
     private void Awake()
     {
         _document = GetComponent<UIDocument>();
            
-        _button = _document.rootVisualElement.Q("StartGameButton") as Button;
-        _button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+        _start_button = _document.rootVisualElement.Q("StartGameButton") as Button;
+        _exit_button = _document.rootVisualElement.Q("ExitGameButton") as Button;
+        _start_button.RegisterCallback<ClickEvent>(OnPlayGameClick);
+        _exit_button.RegisterCallback<ClickEvent>(OnExitGameClick);
     }
 
     private void OnDisable()
     {
-        _button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _start_button.UnregisterCallback<ClickEvent>(OnPlayGameClick);
+        _exit_button.UnregisterCallback<ClickEvent>(OnExitGameClick);
     }
 
     private void OnPlayGameClick(ClickEvent evt)
     {
-        Debug.Log("You pressed the Start Button");
+        Debug.Log("You pressed the Button");
+    }
+
+    private void OnExitGameClick(ClickEvent evt)
+    {
+        Debug.Log("You pressed the Exit Button");
     }
 }
